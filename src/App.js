ecf0e9a53caa9d690dc086bbdd265b668ee59e60
Philan-igmt <philanisithembiso@gmail.com>
2021-01-22 13:55:02 +0000

    all components created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Landing from './components/Landing';
+// import About from './components/About';
+// import Contact from './components/Contact';
+import Nav from './components/Nav';
+// import Testimonials from './components/Testimonials';
+// import Skills from './components/Skills';
+// import Services from './components/Services';
+
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Landing/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/Landing.scss'
+
+const Landing = () => {
+    return (
+        <div className="landing">
+            <h1>Landing page</h1>
+        </div>
+    )
+}
+
+export default Landing
